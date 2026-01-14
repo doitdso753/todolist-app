131dfecd145b985c7ccd192671e1b4bd4c07380c
ParkSoHyeon <dso753@naver.com>
2026-01-14 17:36:02 +0900

    feat: Todo 리스트 페이지 기능 - Create 기능 구현

--- src/components/todo-list/todo-editor.tsx
+++ src/components/todo-list/todo-editor.tsx
@@ -1,16 +1,16 @@
 import { Input } from "@/components/ui/input.tsx";
 import { Button } from "@/components/ui/button.tsx";
 import { useState } from "react";
-import { useCreateTodo } from "@/store/todos.ts";
+import { useCreateTodoMutation } from "@/hooks/mutations/use-create-todo-mutation.ts";
 
 export default function TodoEditor() {
-  const createTodo = useCreateTodo();
+  const { mutate, isPending } = useCreateTodoMutation();
   const [content, setContent] = useState("");
 
   const handleAddTodo = () => {
     if (content.trim() === "") return;
 
-    createTodo(content);
+    mutate(content);
     setContent("");
   }
   return (
@@ -20,7 +20,9 @@ export default function TodoEditor() {
         onChange={(e) => setContent(e.target.value)}
         placeholder="새로운 할 일을 입력하세요 ..."
       />
-      <Button onClick={handleAddTodo}>추가</Button>
+      <Button disabled={isPending} onClick={handleAddTodo}>
+        추가
+      </Button>
     </div>
   );
 }
