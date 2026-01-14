453294f07a2d3c09f5b06efe02c6871f10d9c629
ParkSoHyeon <dso753@naver.com>
2026-01-14 17:22:21 +0900

    feat: 쿼리키 팩토리 방식 적용

--- src/hooks/queries/use-todos-data.ts
+++ src/hooks/queries/use-todos-data.ts
@@ -1,9 +1,10 @@
 import { fetchTodos } from "@/api/fetch-todos.ts";
 import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export function useTodosData() {
   return useQuery({
     queryFn: fetchTodos,
-    queryKey: ["todos"],
+    queryKey: QUERY_KEYS.todo.list,
   });
 }
